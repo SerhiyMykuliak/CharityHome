dafdb5c7526f1efeac703126a9ad78e6e0994bb0
Serhij <serhijko90@gmail.com>
2024-07-18 12:44:29 +0300

    Some refactoring

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,8 @@
 class Admin < ApplicationRecord
   has_secure_password
 
+  validates :name, presence: true, length: {minimum:5, maximum:20}
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP}
+  validates :password_digest, presence: true
   
 end
